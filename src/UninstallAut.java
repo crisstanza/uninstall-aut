b15326265d14ae4b22b522cd8a6c5696a02c4321
Cris Stanza <crisstanza@gmail.com>
2014-05-30 13:53:33 -0300

    Create UninstallAut.java

---
+++ src/UninstallAut.java
@@ -0,0 +1,9 @@
+import com.izforge.izpack.uninstaller.Uninstaller;
+
+public final class UninstallAut {
+
+	public static final void main(final String args[]) {
+		Uninstaller.uninstall(args);
+	}
+
+}
